7f92a9ba45753492e2e13b45cdf42e7cd61140fb
OnafuyeOmolabake <Onafuyeoluwafunmilayo@gmail.com>
2022-09-19 20:02:26 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,7 +8,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int pass[100];
 	int i, sum, n;
@@ -19,7 +19,7 @@ int main (void)
 
 	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand () % 78;
+		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 		if ((2722 - sum) - '0' < 78)
