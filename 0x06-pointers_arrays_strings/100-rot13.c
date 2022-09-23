a4df098a636200d8d176fc1d72ff28caf3ed5ef6
OnafuyeOmolabake <Onafuyeoluwafunmilayo@gmail.com>
2022-09-22 19:41:30 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,7 +14,7 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0, a[j] != '\0'; j++)
+		for (j = 0, ia[j] != '\0'; j++)
 		{
 			if (s[i] == a[j])
 			{
@@ -23,5 +23,6 @@ char *rot13(char *s)
 			}
 		}
 	}
+
 	return (s);
 }
