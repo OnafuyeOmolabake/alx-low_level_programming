2efc7ce4a436c29c53f8916438d9bc7935bda19b
OnafuyeOmolabake <Onafuyeoluwafunmilayo@gmail.com>
2022-09-27 17:39:23 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,11 +8,11 @@
  * Return: the number of bytes in the initial segment
  * of s which consist only of bytes from accept
  */
-unsigned int_strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, f, flag;
 
-	f=0;
+	f = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
